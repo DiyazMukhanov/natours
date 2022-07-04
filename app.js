b86519295975330c9cd55aa18e9c3386bd4d1b51
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-07-04 06:44:52 +0500

    CRUD implementation

---
+++ app.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+
+const app = express();
+
+if(process.env.NODE_ENV === 'development') {
+   app.use(morgan('dev'));
+}
+
+console.log(process.env.NODE_ENV)
+
+
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req, res, next) => {
+   console.log('Hello from middleware');
+   next();
+});
+
+app.use((req, res, next) => {
+   req.requestTime = new Date().toISOString();
+   next();
+})
+
+
+
+
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+
+
+
+
+
+
+
+
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
+
+
