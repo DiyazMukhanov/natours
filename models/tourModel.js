9de9c796677ddf57dff3754792b54d8c452ff49d
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-10-04 18:20:49 +0500

    Наполнение данными

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,16 +4,53 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'Must have duration']
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, 'Must have maxGroupSize']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'Must have difficulty']
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true
     },
     price: {
         type: Number,
         required: [true, 'Tour must have a price']
-    }
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'Must have a description']
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'Must have a image cover']
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
