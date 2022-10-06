71418295418545f13f61f05f9d69f4113a59fe9e
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-10-06 11:47:49 +0500

    Aggregation unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -229,5 +229,57 @@ exports.getTourStats = async (req, res) => {
             message:err
         });
     }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+   try {
+       const year = req.params.year * 1; //2021
+
+       const plan = await Tour.aggregate([
+           {
+               $unwind: '$startDates' //breaking one tour to three by starting dates
+           },
+           {
+               $match: {
+                   startDates: {
+                       $gte: new Date(`${year}-01-01`),
+                       $lte: new Date(`${year}-12-31`)
+                   }
+               }
+           },
+           {
+               $group: {
+                   _id: { $month: '$startDates' },
+                   numTourStarts: { $sum: 1 },
+                   tours: { $push: '$name' }
+               }
+           },
+           {
+               $addFields: { month: '$_id' }
+           },
+           {
+               $project: {
+                   _id: 0 // 1 is to show
+               }
+           },
+           {
+               $sort: { numTourStarts: -1 } //starting from highest number
+           },
+           {
+               $limit: 6
+           }
+       ]);
+
+       res.status(200).json({
+           status:'success',
+           results: plan.length,
+           data: {
+               plan
+           }
+       });
+
+   } catch(err) {
+
+   }
 }
 
