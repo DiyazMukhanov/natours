da4998520722ecaefc613549393b64256821c11b
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-10-06 07:26:48 +0500

    Рефакторинг кода

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -7,73 +8,15 @@ exports.aliasTopTours = (req, res, next) => {
     next();
 }
 
+
 exports.getAllTours = async (req, res) => {
     try{
-        //BUILD THE QUERY
-        //1) FILTERING
-        const queryObj = {...req.query} //by this we create a new obj and not referencing only
-        //Excluding fields from query because we dont have a tour with page etc
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]); //forEach we don't create new Arr. Only modifying. We are cleaning queyObj from fields
-
-        // console.log(req.query, queryObj);
-
-        // const tours = await Tour.find({
-        //     duration: 5,
-        //     difficulty: 'easy'
-        // });
-        //same below
-
-        //2) ADVANCED FILTERING - just do it)
-        console.log(req.query); //{ difficulty: 'easy', duration: { gte: '5' } } if 127.0.0.1:3000/api/v1/tours?duration[gte]=5&difficulty=easy
-        // but we need {difficulty: 'easy', duration: {$gte: 5   }} with $
-
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        console.log(JSON.parse(queryStr));
-
-
-        let query = Tour.find(JSON.parse(queryStr)); // the find method returns the mangoose Query object and this gives us to chain methods
-
-        //3) SORTING
-        //127.0.0.1:3000/api/v1/tours?sort=-price,-ratingsAverage
-        if(req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            //{ sort: '-price,-ratingsAverage' } but mongoose requires string separated by spaces sort: '-price -ratingsAverage'
-            console.log(sortBy)
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('-createdAt');
-        }
-
-        //4) FIELD LIMITING
-        //127.0.0.1:3000/api/v1/tours?fields=name,duration,difficulty,price
-        if(req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-            // query = query.select('name duration difficulty price')
-        } else {
-            query = query.select('-__v'); //exclude __v from the response which is included by mongoose
-        }
-
-        //5) PAGINATION
-        // 127.0.0.1:3000/api/v1/tours?page=2&limit=10
-        //Let's code default values for pagination first below
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 10;
-        const skip = (page - 1) * limit;
-        query = query.skip(skip).limit(limit); //skip first 10 results and give starting from 11
-
-        if(req.query.page) {
-            const numTours = await Tour.countDocuments();
-            if(skip >= numTours) throw new Error('This page does not exist');
-        }
-
-        //6)
-        //127.0.0.1:3000/api/v1/tours?limit=5&sort=-ratingsAverage,price - getting 5 results and sorting by ratings and price
-
-        //EXECUTE THE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate(0)
+        const tours = await features.query;
 
         //SEND RESPONSE
         res.status(200).json({
@@ -89,6 +32,92 @@ exports.getAllTours = async (req, res) => {
             message: err
         })
     }
+
+        //BUILD THE QUERY
+        // //1) FILTERING
+        // const queryObj = {...req.query} //by this we create a new obj and not referencing only
+        // //Excluding fields from query because we dont have a tour with page etc
+        // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        // excludedFields.forEach(el => delete queryObj[el]); //forEach we don't create new Arr. Only modifying. We are cleaning queyObj from fields
+        //
+        // // console.log(req.query, queryObj);
+        //
+        // // const tours = await Tour.find({
+        // //     duration: 5,
+        // //     difficulty: 'easy'
+        // // });
+        // //same below
+        //
+        // //2) ADVANCED FILTERING - just do it)
+        // console.log(req.query); //{ difficulty: 'easy', duration: { gte: '5' } } if 127.0.0.1:3000/api/v1/tours?duration[gte]=5&difficulty=easy
+        // // but we need {difficulty: 'easy', duration: {$gte: 5   }} with $
+        //
+        // let queryStr = JSON.stringify(queryObj);
+        // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        // console.log(JSON.parse(queryStr));
+        //
+        //
+        // let query = Tour.find(JSON.parse(queryStr)); // the find method returns the mangoose Query object and this gives us to chain methods
+
+        // //3) SORTING
+        // //127.0.0.1:3000/api/v1/tours?sort=-price,-ratingsAverage
+        // if(req.query.sort) {
+        //     const sortBy = req.query.sort.split(',').join(' ');
+        //     //{ sort: '-price,-ratingsAverage' } but mongoose requires string separated by spaces sort: '-price -ratingsAverage'
+        //     console.log(sortBy)
+        //     query = query.sort(sortBy);
+        // } else {
+        //     query = query.sort('-createdAt');
+        // }
+
+        // //4) FIELD LIMITING
+        // //127.0.0.1:3000/api/v1/tours?fields=name,duration,difficulty,price
+        // if(req.query.fields) {
+        //     const fields = req.query.fields.split(',').join(' ');
+        //     query = query.select(fields);
+        //     // query = query.select('name duration difficulty price')
+        // } else {
+        //     query = query.select('-__v'); //exclude __v from the response which is included by mongoose
+        // }
+
+        // //5) PAGINATION
+        // // 127.0.0.1:3000/api/v1/tours?page=2&limit=10
+        // //Let's code default values for pagination first below
+        // const page = req.query.page * 1 || 1;
+        // const limit = req.query.limit * 1 || 10;
+        // const skip = (page - 1) * limit;
+        // query = query.skip(skip).limit(limit); //skip first 10 results and give starting from 11
+        //
+        // if(req.query.page) {
+        //     const numTours = await Tour.countDocuments();
+        //     if(skip >= numTours) throw new Error('This page does not exist');
+        // }
+
+        //6)
+        //127.0.0.1:3000/api/v1/tours?limit=5&sort=-ratingsAverage,price - getting 5 results and sorting by ratings and price
+
+        //EXECUTE THE QUERY
+    //     const features = new APIFeatures(Tour.find(), req.query)
+    //         .filter()
+    //         .sort()
+    //         .limitFields()
+    //         .paginate(0)
+    //     const tours = await features.query;
+    //
+    //     //SEND RESPONSE
+    //     res.status(200).json({
+    //         status: 'success',
+    //         results: tours.length,
+    //         data: {
+    //             tours
+    //         }
+    //     })
+    // } catch (err) {
+    //     res.status(404).json({
+    //         status: 'fail',
+    //         message: err
+    //     })
+    // }
 };
 
 exports.getTour = async (req, res) => {
