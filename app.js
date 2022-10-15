a788750b84785295a371f24b1aa03d86abccebd7
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-10-15 16:02:28 +0500

    Рефакторинг ошибок

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 
 const app = express();
 
@@ -16,10 +19,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 
-app.use((req, res, next) => {
-    console.log('Hello from middleware');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from middleware');
+//     next();
+// });
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -34,7 +37,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
+//Unknown route handler
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
+//Error handling middleware
+app.use(globalErrorHandler);
 
 // 4) Start server
 module.exports = app;
