8cf2fdbe39642cf2e048a100c58d37f35f708495
DiyazMukhanov <Diyaz.mukhanov@gmail.com>
2022-10-18 16:19:36 +0500

    Roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,8 @@ exports.signup = catchAsync(async (req, res, next) => {
         email: req.body.email,
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm,
-        passwordChangedAt: req.body.passwordChangedAt
+        passwordChangedAt: req.body.passwordChangedAt,
+        role: req.body.role
     });
 
     const token = signToken(newUser._id);
@@ -82,3 +83,14 @@ exports.protect = catchAsync(async(req, res, next) => {
     req.user = freshUser;
     next();
 });
+
+//(...roles) will create an array of all roles for example ['admin', 'lead-guide']
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new AppError('You do not have permission', 403));
+        }
+
+        next();
+    }
+}
